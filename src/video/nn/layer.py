64d0f5b76bcc953b580a73078b2e04aa0cd8e277
atksh <31435204+atksh@users.noreply.github.com>
2022-10-16 23:45:43 +0900

    fix

--- src/video/nn/layer.py
+++ src/video/nn/layer.py
@@ -27,14 +27,29 @@ def ckpt_forward(func):
 
 
 def ckpt_seq_forward(func):
-    def wrapper(x):
-        if x.requires_grad:
+    def wrapper(*args, **kwargs):
+        all_inputs = args + tuple(kwargs.values())
+        batch_size = None
+        for a in all_inputs:
+            if torch.is_tensor(a):
+                batch_size = a.shape[0]
+                break
+        if batch_size is not None and any(
+            torch.is_tensor(x) and x.requires_grad for x in all_inputs
+        ):
+            new_args = []
+            new_kwargs = []
+            for i in range(batch_size):
+                new_args.append([a[[i]] if torch.is_tensor(a) else a for a in args])
+                new_kwargs.append(
+                    {k: v[[i]] if torch.is_tensor(v) else v for k, v in kwargs.items()}
+                )
             out = []
-            for i in range(x.shape[0]):
-                out.append(checkpoint(func, x[[i]]))
+            for a, k in zip(new_args, new_kwargs):
+                out.append(checkpoint(func, *a, **k))
             return torch.cat(out, dim=0)
         else:
-            return func(x)
+            return func(*args, **kwargs)
 
     return wrapper
 
