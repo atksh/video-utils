5745b3abf99ba04610712c1ed2e3e37aa6226878
atksh <31435204+atksh@users.noreply.github.com>
2022-10-17 20:17:09 +0900

    fix

--- predict.py
+++ predict.py
@@ -14,12 +14,11 @@ from video.dataset import VideoDatasetForInference
 
 if __name__ == "__main__":
     resolusion = "640:360"
-    resolusion = "256:144"
     batch_size = 1
     fps = 30
-    skip_rate = 1
+    skip_rate = 6
     max_len = 16
-    n_steps = 1
+    n_steps = 4
     num_mix = 4
     last_dim = 64
 
