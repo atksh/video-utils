73cfffddc33fe6e6c1e7ca4d0286d4b66ee6bbb4
atksh <31435204+atksh@users.noreply.github.com>
2022-10-17 20:15:04 +0900

    fix

--- train.py
+++ train.py
@@ -10,17 +10,16 @@ if __name__ == "__main__":
     cache_dir = "cache"
     path = ["video.mp4"]
     total_batch_size = 32
-    batch_size = 8
+    batch_size = 1
     num_workers = 16
 
     max_epochs = 1000
 
     resolusion = "640:360"
-    resolusion = "256:144"
     fps = 30
-    skip_rate = 1
+    skip_rate = 6
     max_len = 16
-    n_steps = 1
+    n_steps = 4
     num_mix = 4
     last_dim = 64
 
