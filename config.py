b2bc58cd93491d32c516f10777b70773f6662c8c
atksh <31435204+atksh@users.noreply.github.com>
2022-10-19 20:02:49 +0900

    fix

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 resolution = "426:240"
 skip_rate = 4
-max_len = 32
+max_len = 16
 
 num_workers = 16
 total_batch_size = 32
@@ -9,10 +9,10 @@ predict_batch_size = 4
 fps = 30
 
 backbone_feat_dims = [80, 160, 320, 640]
-front_feat_dims = [32, 32, 64, 96]
-enc_num_heads = [1, 1, 2, 3]
-enc_num_layers = [2, 4, 6, 8]
-dec_num_heads = [1, 1, 1, 2]
+front_feat_dims = [32, 64, 128, 256]
+enc_num_heads = [1, 2, 4, 8]
+enc_num_layers = [3, 6, 9, 12]
+dec_num_heads = [1, 1, 2, 4]
 dec_num_layers = [1, 2, 3, 4]
 n_steps = 4
 last_dim = 32
