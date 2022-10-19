cfb2c435837ed33a73b1e9c1c380e6130c0005c8
atksh <31435204+atksh@users.noreply.github.com>
2022-10-20 00:45:31 +0900

    fix

--- train.py
+++ train.py
@@ -49,6 +49,8 @@ if __name__ == "__main__":
         enable_checkpointing=True,
         gradient_clip_val=1.0,
         gradient_clip_algorithm="norm",
-        limit_train_batches=0.5 / skip_rate,
+        limit_train_batches=1.0 / skip_rate,
+        limit_val_batches=1.0 / skip_rate,
+        limit_test_batches=1.0 / skip_rate,
     )
     trainer.fit(model=model, datamodule=dl)
