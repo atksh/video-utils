855fa75c55370e3c8d1553f526e91bd82d30bc88
atksh <31435204+atksh@users.noreply.github.com>
2022-10-20 00:20:03 +0900

    fix

--- src/video/nn/loss.py
+++ src/video/nn/loss.py
@@ -58,36 +58,39 @@ class MSSSIML1Loss(nn.Module):
         x = self.to_image(x)
         y = self.to_image(y)
 
-        mux = F.conv2d(x, self.g_masks, groups=3, padding=self.pad)
-        muy = F.conv2d(y, self.g_masks, groups=3, padding=self.pad)
+        with torch.cuda.amp.autocast(enabled=False):
+            mux = F.conv2d(x, self.g_masks, groups=3, padding=self.pad)
+            muy = F.conv2d(y, self.g_masks, groups=3, padding=self.pad)
 
-        mux2 = mux * mux
-        muy2 = muy * muy
-        muxy = mux * muy
+            mux2 = mux * mux
+            muy2 = muy * muy
+            muxy = mux * muy
 
-        sigmax2 = F.conv2d(x * x, self.g_masks, groups=3, padding=self.pad) - mux2
-        sigmay2 = F.conv2d(y * y, self.g_masks, groups=3, padding=self.pad) - muy2
-        sigmaxy = F.conv2d(x * y, self.g_masks, groups=3, padding=self.pad) - muxy
+            sigmax2 = F.conv2d(x * x, self.g_masks, groups=3, padding=self.pad) - mux2
+            sigmay2 = F.conv2d(y * y, self.g_masks, groups=3, padding=self.pad) - muy2
+            sigmaxy = F.conv2d(x * y, self.g_masks, groups=3, padding=self.pad) - muxy
 
-        # l(j), cs(j) in MS-SSIM
-        l = (2 * muxy + self.C1) / (mux2 + muy2 + self.C1)  # [B, 15, H, W]
-        cs = (2 * sigmaxy + self.C2) / (sigmax2 + sigmay2 + self.C2)
+            # l(j), cs(j) in MS-SSIM
+            l = (2 * muxy + self.C1) / (mux2 + muy2 + self.C1)  # [B, 15, H, W]
+            cs = (2 * sigmaxy + self.C2) / (sigmax2 + sigmay2 + self.C2)
 
-        lM = l[:, -1, :, :] * l[:, -2, :, :] * l[:, -3, :, :]
-        PIcs = cs.prod(dim=1)
+            lM = l[:, -1, :, :] * l[:, -2, :, :] * l[:, -3, :, :]
+            PIcs = cs.prod(dim=1)
 
-        loss_ms_ssim = 1 - lM * PIcs  # [B, H, W]
+            loss_ms_ssim = 1 - lM * PIcs  # [B, H, W]
 
-        loss_l1 = F.l1_loss(x, y, reduction="none")  # [B, 3, H, W]
-        # average l1 loss in 3 channels
-        gaussian_l1 = F.conv2d(
-            loss_l1,
-            self.g_masks.narrow(dim=0, start=-3, length=3),
-            groups=3,
-            padding=self.pad,
-        ).mean(
-            1
-        )  # [B, H, W]
-        loss_mix = self.alpha * loss_ms_ssim + (1 - self.alpha) * gaussian_l1 / self.DR
+            loss_l1 = F.l1_loss(x, y, reduction="none")  # [B, 3, H, W]
+            # average l1 loss in 3 channels
+            gaussian_l1 = F.conv2d(
+                loss_l1,
+                self.g_masks.narrow(dim=0, start=-3, length=3),
+                groups=3,
+                padding=self.pad,
+            ).mean(
+                1
+            )  # [B, H, W]
+            loss_mix = (
+                self.alpha * loss_ms_ssim + (1 - self.alpha) * gaussian_l1 / self.DR
+            )
 
-        return loss_mix.mean()
+            return loss_mix.mean()
