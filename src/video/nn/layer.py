da3059de13a9fe0934b15a51f7a6a27504f1bdb4
atksh <31435204+atksh@users.noreply.github.com>
2022-10-19 23:20:17 +0900

    fix

--- src/video/nn/layer.py
+++ src/video/nn/layer.py
@@ -1,6 +1,5 @@
 import revlib
 import torch
-from functorch.compile import memory_efficient_fusion
 from torch import nn
 from torch.nn import functional as F
 
@@ -10,7 +9,7 @@ NEG_INF = -5000.0
 class ReversibleSequential(nn.Module):
     def __init__(self, layers, split_dim):
         super().__init__()
-        layers = [memory_efficient_fusion(layer) for layer in layers]
+        layers = [torch.jit.script(layer) for layer in layers]
         self.split_dim = split_dim
         self.layers = revlib.ReversibleSequential(*layers, split_dim=split_dim)
 
