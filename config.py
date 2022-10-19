73a5e6e39e6d7455a2b386d614c7ccf8d7c68265
atksh <31435204+atksh@users.noreply.github.com>
2022-10-19 14:14:27 +0900

    fix

--- config.py
+++ config.py
@@ -9,10 +9,10 @@ predict_batch_size = 2
 fps = 30
 
 backbone_feat_dims = [80, 160, 320, 640]
-front_feat_dims = [32, 32, 64, 128]
-enc_num_heads = [1, 1, 2, 4]
+front_feat_dims = [32, 32, 64, 96]
+enc_num_heads = [1, 1, 2, 3]
 enc_num_layers = [4, 4, 32, 4]
-dec_num_heads = [1, 1, 2, 4]
+dec_num_heads = [1, 1, 2, 3]
 dec_num_layers = [2, 2, 16, 2]
 n_steps = 4
 last_dim = 32
