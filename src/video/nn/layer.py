a361b4b5731926bfce252a0ca6276d0cf60d7dd3
atksh <31435204+atksh@users.noreply.github.com>
2022-10-21 14:47:27 +0900

    fix

--- src/video/nn/layer.py
+++ src/video/nn/layer.py
@@ -2,6 +2,7 @@ import revlib
 import torch
 from torch import nn
 from torch.nn import functional as F
+from torchvision.ops import StochasticDepth
 
 NEG_INF = -5000.0
 
@@ -93,6 +94,82 @@ class DualScaleUpsample(nn.Module):
         return x
 
 
+class LayerScaler(nn.Module):
+    def __init__(self, s: float, dim: int):
+        super().__init__()
+        self.gamma = nn.Parameter(s * torch.ones((dim)), requires_grad=True)
+
+    def forward(self, x):
+        return self.gamma[None, ..., None, None] * x
+
+
+class LayerNorm2D(nn.LayerNorm):
+    def __init__(self, num_channels, eps=1e-5, affine=True):
+        super().__init__(num_channels, eps=eps, elementwise_affine=affine)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = x.permute(0, 2, 3, 1)
+        x = F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        x = x.permute(0, 3, 1, 2)
+        return x
+
+
+class SELayer(nn.Module):
+    def __init__(self, dim, reduction=16):
+        super().__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        intermed_dim = max(dim // reduction, 2)
+        self.fc = nn.Sequential(
+            nn.Linear(dim, intermed_dim),
+            nn.SiLU(),
+            nn.Linear(intermed_dim, dim),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
+
+class Block(nn.Module):
+    def __init__(
+        self,
+        dim: int,
+        layer_scaler_init_value=1e-6,
+        drop_p=0.1,
+    ):
+        super().__init__()
+        self.conv = nn.Conv2d(
+            dim,
+            dim,
+            groups=dim,
+            kernel_size=7,
+            padding=3,
+            bias=False,
+        )
+        self.ln = LayerNorm2D(dim)
+        self.mlp = nn.Sequential(
+            nn.Conv2d(dim, dim * 4, 1),
+            nn.SiLU(),
+            SELayer(dim * 4),
+            nn.Conv2d(dim * 4, dim, 1, bias=False),
+        )
+        self.layer_scaler = LayerScaler(layer_scaler_init_value, dim)
+        self.drop_path = StochasticDepth(drop_p, mode="batch")
+
+    def forward(self, x):
+        resid = x
+        x = self.conv(x)
+        x = self.ln(x)
+        x = self.mlp(x)
+        x = self.layer_scaler(x)
+        x = self.drop_path(x)
+        x += resid
+        return x
+
+
 class Layer2D(nn.Module):
     def __init__(self, module):
         super().__init__()
@@ -121,36 +198,6 @@ class SimpleLayer2D(nn.Module):
         x = self.module(x)
         x = self.to_video(x, bsz)
         return x
-
-
-class LayerNorm2D(nn.LayerNorm):
-    def __init__(self, num_channels, eps=1e-5, affine=True):
-        super().__init__(num_channels, eps=eps, elementwise_affine=affine)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = x.permute(0, 2, 3, 1)
-        x = F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
-        x = x.permute(0, 3, 1, 2)
-        return x
-
-
-class SELayer(nn.Module):
-    def __init__(self, dim, reduction=8):
-        super().__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        intermed_dim = max(dim // reduction, 2)
-        self.fc = nn.Sequential(
-            nn.Linear(dim, intermed_dim, bias=False),
-            NonLinear(),
-            nn.Linear(intermed_dim, dim, bias=False),
-            Sigmoid(),
-        )
-
-    def forward(self, x):
-        b, c, _, _ = x.size()
-        y = self.avg_pool(x).view(b, c)
-        y = self.fc(y).view(b, c, 1, 1)
-        return x * y.expand_as(x)
 
 
 class LRASPP(nn.Module):
