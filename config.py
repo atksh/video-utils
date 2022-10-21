6833105e55409991d89d598ddbddd72c607fac2f
atksh <31435204+atksh@users.noreply.github.com>
2022-10-21 18:52:47 +0900

    fix

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ max_len = 32
 
 num_workers = 8
 total_batch_size = 32
-train_batch_size = 8
+train_batch_size = 16
 predict_batch_size = 4
 fps = 30
 n_steps = 1
@@ -13,6 +13,6 @@ n_steps = 1
 in_dim = 3
 stem_dim = 16
 widths = [16, 32, 64, 128, 256]
-depths = [1, 1, 1, 1, 1]
+depths = [2, 8, 8, 2, 2]
 heads = [1, 1, 2, 4, 8]
 drop_p = 0.0
