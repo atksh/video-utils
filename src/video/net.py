4cd18b689c0b72085b3842c45d4b642c4d2db20b
atksh <31435204+atksh@users.noreply.github.com>
2022-10-22 12:30:44 +0900

    fix

--- src/video/net.py
+++ src/video/net.py
@@ -155,8 +155,7 @@ class Model(pl.LightningModule):
     def configure_optimizers(self):
         return AdaBelief(
             self.parameters(),
-            lr=1e-5,
+            lr=4e-5,
             weight_decay=1e-4,
-            eps=1e-12,
             print_change_log=False,
         )
