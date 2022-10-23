e3e6cfb30e21d12db24776c52fc9212f9cc88267
atksh <31435204+atksh@users.noreply.github.com>
2022-10-23 19:53:33 +0900

    fix

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ skip_rate = 5
 max_len = 8
 num_workers = 8
 
-train_batch_size = 8
+train_batch_size = 4
 predict_batch_size = 1
 accumulate_grad_batchs = 1
 fps = 30
@@ -11,11 +11,11 @@ n_steps = 1
 
 
 in_ch = out_ch = 3
-widths = [16, 32, 48, 64]
+widths = [32, 64, 128, 256]
 depths = [1, 2, 3, 4]
-heads = [1, 1, 2, 4]
-head_widths = [16, 16, 16, 16]
-block_sizes = [16, 16, 8, 8]
+heads = [1, 2, 4, 8]
+head_widths = [32, 32, 32, 32]
+block_sizes = [16, 8, 4, 4]
 kernel_sizes = [7, 7, 5, 3]
 dec_depths = [1, 1, 1, 1]
 resolution_scale = 2
